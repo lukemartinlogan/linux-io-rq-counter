0602bd24af24f879b799710a688fe9296c12e4ca
lukemartinlogan <lukemartinlogan@gmail.com>
2020-07-27 14:59:01 -0500

    Actually add renamed files

---
+++ linux-io-rq-counter.h
@@ -0,0 +1,17 @@
+
+#ifndef LINUX_IO_REQUEST_COUNTER_H
+#define LINUX_IO_REQUEST_COUNTER_H
+
+static struct km_request {
+	int code;
+	union {
+		void *buf;
+		int val;
+	} data;
+};
+
+void init_counter_syscalls(void);
+int mount_counter(char *dev);
+int get_num_io_rqs(char *dev);
+
+#endif
---
+++ mount-counter.c
@@ -0,0 +1,19 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <asm/types.h>
+#include <sys/socket.h>
+#include <linux/netlink.h>
+#include <linux-io-rq-counter.h>
+
+int main(int argc, char **argv)
+{
+	if(argc != 2) {
+		printf("USAGE: ./mount-counter [path/to/device/file]\n");
+		exit(1);
+	}
+	char *dev = argv[1];
+	
+	init_counter_syscalls();
+	mount_counter(dev);
+}
